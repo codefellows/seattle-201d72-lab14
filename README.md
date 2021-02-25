9e817bcb16f13713ebd75323a6e4aa0610358f60
Simone Odegard <Simone.Odegard@gmail.com>
2021-02-25 14:29:55 -0800

    added to catalog and app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # seattle-201d72-lab14
 Bus Mall Store
+
+1:30-2:30 - Driver: Simone, Navigator: Jessi and Mal
--- js/catalog.js
+++ js/catalog.js
@@ -47,6 +47,7 @@ function handleSubmit(event) {
 
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
+  for (i = 0; i < items.length; i++);
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
